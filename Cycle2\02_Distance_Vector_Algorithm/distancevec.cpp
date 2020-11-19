dcde2780fe7f1392b5e84d26541845543f665476
Shalini1515 <55015107+Shalini1515@users.noreply.github.com>
2020-11-19 12:29:49 +0530

    Update distancevec.cpp

--- Cycle2\02_Distance_Vector_Algorithm/distancevec.cpp
+++ Cycle2\02_Distance_Vector_Algorithm/distancevec.cpp
@@ -10,7 +10,7 @@ class router
     router( )
     {
       for(int i=0;i<MAX;i++) 
-      table_old[i]=table_new[i]=99;
+         table_old[i]=table_new[i]=99;
     }
     void copy( )
     {
@@ -23,49 +23,50 @@ class router
     int equal( ) 
     {
         for(int i=0;i<n;i++)
-        if(table_old[i]!=table_new[i]||adj_new[i]!=adj_old[i])
-         return 0;
+         if(table_old[i]!=table_new[i]||adj_new[i]!=adj_old[i])
+            return 0;
          return 1;
     }
     void input(int j) 
     {
-       cout<<"Enter 1 if the corresponding router is adjacent to router"<<(char)('A'+j)<<" else enter 99: "<<endl<<" ";
+        cout<<"Enter 1 if the corresponding router is adjacent to router"<<(char)('A'+j)<<" else enter 99: "<<endl<<" ";
         for(int i=0;i<n;i++)
-        if(i!=j) 
-          cout<<(char)('A'+i)<<" ";
-        cout<<"\nEnter matrix:";
-        for(i=0;i<n;i++) {
-          if(i==j)
-             table_new[i]=0;
-          else
-             cin>>table_new[i];
-        adj_new[i]= (char)('A'+i);
+         if(i!=j) 
+            cout<<(char)('A'+i)<<" ";
+         cout<<"\nEnter matrix:";
+         for(i=0;i<n;i++) 
+         {
+            if(i==j)
+               table_new[i]=0;
+            else
+               cin>>table_new[i];
+            adj_new[i]= (char)('A'+i);
+         }
+         cout<<endl;
      }
-     cout<<endl;
-}
-void display()
-{
-  cout<<"\nDestination Router: ";
-  for(int i=0;i<n;i++)
-    cout<<(char)('A'+i)<<" ";
-  cout<<"\nOutgoing Line: ";
-  for(i=0;i<n;i++) 
-    cout<<adj_new[i]<<" ";
-  cout<<"\nHop Count: ";
-  for(i=0;i<n;i++) 
-    cout<<table_new[i]<<" ";
-}
-void build(int j) 
-{
-  for(int i=0;i<n;i++)
-  for(int k=0;(i!=j)&&(k<n);k++)
-    if(table_old[i]!=99)
-  if((table_new[i]+r[i].table_new[k])<table_new[k]) 
-  {
-    table_new[k]=table_new[i]+r[i].table_new[k];
-    adj_new[k]=(char)('A'+i);
-  }
-}
+   void display()
+   {
+       cout<<"\nDestination Router: ";
+       for(int i=0;i<n;i++)
+          cout<<(char)('A'+i)<<" ";
+       cout<<"\nOutgoing Line: ";
+       for(i=0;i<n;i++) 
+         cout<<adj_new[i]<<" ";
+       cout<<"\nHop Count: ";
+       for(i=0;i<n;i++) 
+         cout<<table_new[i]<<" ";
+   }
+   void build(int j) 
+   {
+      for(int i=0;i<n;i++)
+         for(int k=0;(i!=j)&&(k<n);k++)
+            if(table_old[i]!=99)
+               if((table_new[i]+r[i].table_new[k])<table_new[k]) 
+               {
+                  table_new[k]=table_new[i]+r[i].table_new[k];
+                  adj_new[k]=(char)('A'+i);
+               }
+    }
 } r[10];
 void build_table( )
 {
@@ -96,6 +97,6 @@ void main()
     cout<<"Router Table entries for router "<<(char)('A'+i)<<":-";
     r[i].display();
     cout<<endl<<endl;
-   }
+  }
    getch();
 }
